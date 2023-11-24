7726474965eef78367925a2c09739db9075da199
Federico-Maiocchi <maiocchifederico@gmail.com>
2023-11-24 16:27:58 +0100

    ADD: push file todos.json in server.php

--- server.php
+++ server.php
@@ -1,25 +1,34 @@
 <?php
 // in questo file stampiamo il file J Son
 
+// prendiamo i nostri file direttamente dal todos.json
+// - metodo per prendere e leggere il contenuto del file
+$todos_json = file_get_contents('./todos.json');
+
+// json decodificato
+$todos = json_decode($todos_json, true);
+
+
+
 // traferiamo gli stessi dati che abbiamo nel client
-$todos = [
-    [
-    'text' => 'PHP',
-    'done' => false
-    ],
-    [
-    'text' => 'JavaScript',
-    'done' => false
-    ],
-    [
-    'text' => 'HTML',
-    'done' => true
-    ],
-    [
-    'text' => 'CSS',
-    'done' => true
-    ]
-];
+// $todos = [
+//     [
+//     'text' => 'PHP',
+//     'done' => false
+//     ],
+//     [
+//     'text' => 'JavaScript',
+//     'done' => false
+//     ],
+//     [
+//     'text' => 'HTML',
+//     'done' => true
+//     ],
+//     [
+//     'text' => 'CSS',
+//     'done' => true
+//     ]
+// ];
 
 
 // impostazione del header
